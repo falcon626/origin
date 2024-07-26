5d750f0b63eccb9734add7fed6b2eac610f2de06
falcon626 <626hayato@gmail.com>
2024-07-26 10:02:39 +0900

    配布用　タイマー追加後
    切替可能

---
+++ Timer.cpp
@@ -0,0 +1,64 @@
+﻿#include "Timer.h"
+
+void Timer::Start() noexcept
+{
+    if (!m_isRunning)
+	{
+        auto now         = std::chrono::steady_clock::now();
+        if (m_startTime != std::chrono::steady_clock::time_point()) m_startTime += (now - m_stopTime);
+        else m_startTime = now;
+        m_isRunning      = true;
+    }
+}
+
+void Timer::Stop() noexcept
+{
+    if (m_isRunning)
+	{
+        m_stopTime  = std::chrono::steady_clock::now();
+        m_isRunning = false;
+    }
+}
+
+void Timer::Restart() noexcept
+{
+    Stop();
+    m_startTime = std::chrono::steady_clock::now();
+    m_isRunning = true;
+}
+
+
+
+void Timer::Resume() noexcept
+{
+    if (!m_isRunning) 
+	{
+        m_startTime += (std::chrono::steady_clock::now() - m_stopTime);
+        m_isRunning  = true;
+    }
+}
+
+
+
+void Timer::Reset() noexcept
+{
+	m_startTime = std::chrono::steady_clock::time_point();
+	m_stopTime = std::chrono::steady_clock::time_point();
+	m_isRunning = false;
+}
+
+uint32_t const Timer::ElapsedSeconds() const noexcept
+{
+#if TimerSwitcher
+	auto endTime = m_isRunning ? std::chrono::steady_clock::now() : m_stopTime;
+	return static_cast<uint32_t>(std::chrono::duration_cast<std::chrono::seconds>(endTime - m_startTime).count());
+#else
+    ++m_frameCounter;
+    if (m_frameCounter >= Fps)
+    {
+        m_frameCounter = NULL;
+        ++m_second;
+    }
+    return m_second;
+#endif // TimerSwitcher
+}
---
+++ Timer.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+class Timer
+{
+public:
+    Timer () noexcept : m_isRunning(false) {}
+	~Timer() noexcept = default;
+
+    void Start() noexcept;
+
+    void Stop() noexcept;
+
+    void Restart() noexcept;
+
+    void Resume() noexcept;
+
+	void Reset() noexcept;
+
+    uint32_t const ElapsedSeconds() const noexcept;
+
+private:
+    std::chrono::steady_clock::time_point m_startTime;
+    std::chrono::steady_clock::time_point m_stopTime;
+    bool m_isRunning;
+
+#define TimerSwitcher
+
+#if TimerSwitcher
+    const uint32_t Fps = 60;
+    uint32_t m_frameCounter = NULL;
+    uint32_t m_second = NULL;
+#endif // TimerSwitcher
+
+};
