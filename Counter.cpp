827fb3c67601b91bedbe8d315408fd80539f1d35
falcon626 <626hayato@gmail.com>
2024-07-26 10:17:54 +0900

    配布用　タイマー付き
    一応カウンターも追加

---
+++ Counter.cpp
@@ -0,0 +1,81 @@
+﻿#include "Counter.h"
+#include "../../Data/ResourceManager.h"
+#include "../../Data/BinaryAccessor.hpp"
+#include "../../Utility/UtilityDefault.hxx"
+
+Counter::Counter() noexcept
+	: m_oneSize    (Def::IntNull)
+	, m_computed   (Def::IntNull)
+	, m_countNumber(Def::IntNull)
+{ Init(); }
+
+void Counter::Init()
+{
+	std::vector<int>  parameter;
+	auto counter(Def::SizTNull);
+
+	{
+#if _DEBUG
+		const auto IsAssert = DATA.Load("Asset/Data/CounterParameter/Initial_Int.dat", parameter, counter);
+		_ASSERT_EXPR(IsAssert, L"BinaryData Not Found");
+#else
+		DATA.Load("Asset/Data/CounterParameter/Initial_Int.dat", parameter, counter);
+#endif // _DEBUG
+	}
+
+	m_color    = Def::Color;
+	m_computed = parameter[--counter];
+}
+
+void Counter::DrawSpriteCounter() noexcept
+{
+	auto x(Def::FloatNull);
+	for (decltype(auto) digitRect : m_rect)
+	{
+		KdShaderManager::Instance().m_spriteShader.DrawTex(m_spTex, static_cast<int>(m_pos.x - (x * digitRect.height)), static_cast<int>(m_pos.y), m_oneSize, m_oneSize, &digitRect, &m_color);
+		++x;
+	}
+}
+
+void Counter::Update() noexcept
+{
+	SetCounterRect();
+}
+
+void Counter::SetCounterParameter(const int& countNumber, const Math::Vector2& pos) noexcept
+{
+	m_countNumber = countNumber;
+	m_pos = pos;
+}
+
+void Counter::SetCounterParameter(const int& countNumber, const Math::Vector2& pos, const Math::Color& color) noexcept
+{
+	m_countNumber = countNumber;
+	m_pos = pos;
+	m_color = color;
+}
+
+void Counter::SetCounterParameter(const int& countNumber, const Math::Vector2& pos, const int& rectSize) noexcept
+{
+	m_countNumber = countNumber;
+	m_pos = pos;
+	m_oneSize = rectSize;
+}
+
+void Counter::SetCounterParameter(const int& countNumber, const Math::Vector2& pos, const int& rectSize, const Math::Color& color) noexcept
+{
+	m_countNumber = countNumber;
+	m_pos = pos;
+	m_oneSize = rectSize;
+	m_color = color;
+}
+
+void Counter::SetCounterRect() noexcept
+{
+	auto hundred = m_countNumber / (m_computed * m_computed);
+	auto teen = (m_countNumber / m_computed) % m_computed;
+	auto one = m_countNumber % m_computed;
+	m_rect[static_cast<size_t>(Digit::Hundreds)] = { m_oneSize * hundred, Def::IntNull, m_oneSize, m_oneSize };
+	m_rect[static_cast<size_t>(Digit::Tens)]     = { m_oneSize * teen, Def::IntNull, m_oneSize, m_oneSize };
+	m_rect[static_cast<size_t>(Digit::Ones)]     = { m_oneSize * one, Def::IntNull, m_oneSize, m_oneSize };
+}
---
+++ Counter.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+class Counter
+{
+public:
+	Counter ()noexcept;
+	~Counter()noexcept = default;
+
+	void DrawSpriteCounter() noexcept;
+	void Update() noexcept;
+	
+	void SetTex(const std::shared_ptr<KdTexture>& spTex) noexcept { m_spTex = spTex; m_oneSize = m_spTex->GetHeight(); }
+
+	void SetCounterParameter(const int& countNumber, const Math::Vector2& pos) noexcept;
+	void SetCounterParameter(const int& countNumber, const Math::Vector2& pos, const Math::Color& color) noexcept;
+	void SetCounterParameter(const int& countNumber, const Math::Vector2& pos, const int& rectSize) noexcept;
+	void SetCounterParameter(const int& countNumber, const Math::Vector2& pos, const int& rectSize, const Math::Color& color) noexcept;
+
+private:
+
+	enum class Digit : size_t
+	{
+		Ones,
+		Tens,
+		Hundreds,
+		Max
+	};
+
+	void Init();
+	void SetCounterRect() noexcept;
+
+	std::array<Math::Rectangle, static_cast<size_t>(Digit::Max)> m_rect;
+	std::shared_ptr<KdTexture> m_spTex;
+	Math::Vector2 m_pos;
+	Math::Color m_color;
+
+	int m_oneSize;
+	int m_computed;
+	int m_countNumber;
+};
